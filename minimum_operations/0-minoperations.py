da2c5885e5c157753cfb5d1777724cbd64ecc93a
OIgboerika <o.igboerika@alustudent.com>
2024-01-20 05:04:29 -0800

    operandi

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
 def minOperations(n):
-    if n == 0:
+    """ Minimum Operations Function. """
+    if n <= 1:
         return 0
-    if n == 1:
-        return 1
-    operations = 0
-    while n > 1:
-        if n % 2 == 0:
-            n = n // 2
-            operations += 1
-        else:
-            n = n - 1
-            operations += 1
-    return operations + 1
+
+    # Set the minimum operation variable to 0
+    min_operations = 0
+    # Set the current length variable to 1 as we start with 1 character 'H'
+    current_length = 1
+    # Set the clipboard variable to 0 as we are not copying anything yet
+    clipboard = 0
+
+    # Create an array dp where dp[i] represents the minimum number of operations required to create i characters
+    dp = [0 for i in range(n + 1)]
+
+    # Loop until current length is equal to n
+    while current_length < n:
+        # If n is divisible by current length, we can copy all
+        if n % current_length == 0:
+            # This is the only time we can copy
+            clipboard = current_length
+            # Minimum operation is incremented by 1 because we copied
+            min_operations += 1
+
+        # Paste the clipboard
+        current_length += clipboard
+        # Minimum operation is incremented by 1 because we pasted
+        min_operations += 1
+
+        # Update the dp array
+        dp[current_length] = min_operations
+
+    # Return minimum operation
+    return dp[n]
